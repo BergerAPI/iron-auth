20b2b06cfcb0ebd1659db6ff3d19d83931e1ee7d
BergerAPI <bergerbtw@gmail.com>
2024-10-03 18:01:13 +0200

    feat: very, very basic login

--- cmd/entry.go
+++ cmd/entry.go
@@ -1,12 +1,49 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
 
 func main() {
-	app := fiber.New()
+	// Create a new engine
+	engine := html.New("./public", ".html")
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	// Creating a fiber app with all views
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Get("/login", func(ctx *fiber.Ctx) error {
+		clientId := ctx.Query("client_id", "")
+		redirectUri := ctx.Query("redirect_uri", "")
+		state := ctx.Query("state", "")
+		success := ctx.Query("success", "true")
+
+		return ctx.Render("login", fiber.Map{
+			"ClientId":    clientId,
+			"RedirectUri": redirectUri,
+			"State":       state,
+			"Success":     success,
+		})
+	})
+
+	app.Post("/login", func(ctx *fiber.Ctx) error {
+		// Parsing the form data
+		email := ctx.FormValue("email", "")
+		password := ctx.FormValue("password", "")
+
+		// Form data also included information about the oauth2 session
+		// if it was provided by the query parameters in GET /login
+		clientId := ctx.FormValue("client_id", "")
+		redirectUri := ctx.FormValue("redirect_uri", "")
+		state := ctx.FormValue("state", "")
+
+		if email != "test@niclas.lol" && password != "test123" {
+			return ctx.Redirect("/login?success=false&client_id="+clientId+"&redirect_uri="+redirectUri+"&state="+state, 302)
+		}
+
+		return ctx.SendString(email + " " + password)
 	})
 
 	err := app.Listen(":3000")
