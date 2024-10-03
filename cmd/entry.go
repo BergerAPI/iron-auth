5f6709c31d913ee6bd74e8a7b39ce7e14419ac41
BergerAPI <bergerbtw@gmail.com>
2024-10-03 18:21:45 +0200

    feat: json web tokens

--- cmd/entry.go
+++ cmd/entry.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
 )
 
 func main() {
@@ -12,6 +14,10 @@ func main() {
 	// Creating a fiber app with all views
 	app := fiber.New(fiber.Config{
 		Views: engine,
+	})
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello World!")
 	})
 
 	app.Get("/login", func(ctx *fiber.Ctx) error {
@@ -40,10 +46,34 @@ func main() {
 		state := ctx.FormValue("state", "")
 
 		if email != "test@niclas.lol" && password != "test123" {
-			return ctx.Redirect("/login?success=false&client_id="+clientId+"&redirect_uri="+redirectUri+"&state="+state, 302)
+			return ctx.Redirect("/login?success=false&client_id=" + clientId + "&redirect_uri=" + redirectUri + "&state=" + state)
 		}
 
-		return ctx.SendString(email + " " + password)
+		// Token and cookie shall become invalid or be removed in 30 days from now
+		expiration := time.Now().Add(30 * 24 * time.Hour)
+
+		// Create a new token object, specifying signing method and the claims
+		// you would like it to contain.
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"userId": "foobar",
+			"exp":    expiration.Unix(),
+		})
+
+		// Sign and get the complete encoded token as a string using the secret
+		tokenString, err := token.SignedString([]byte("this-needs-to-be-replaced"))
+
+		if err != nil {
+			return ctx.Redirect("/login?success=false&client_id=" + clientId + "&redirect_uri=" + redirectUri + "&state=" + state)
+		}
+
+		// Create and set the cookie for storing the session
+		cookie := new(fiber.Cookie)
+		cookie.Name = "auth"
+		cookie.Value = tokenString
+		cookie.Expires = expiration
+		ctx.Cookie(cookie)
+
+		return ctx.Redirect("/")
 	})
 
 	err := app.Listen(":3000")
